43265b5d37e6615f73e4c36939a5e6452ca07d60
lizapira <lp47657@ubt-uni.net>
2022-05-22 23:04:44 +0200

    update newarrival.jsx

--- user/ecom/src/components/home/NewArrival.jsx
+++ user/ecom/src/components/home/NewArrival.jsx
@@ -5,10 +5,25 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 class NewArrival extends Component {
+
+  constructor(props){
+    super(props);
+    this.next=this.next.bind(this);
+    this.previous=this.previous.bind(this)
+}
+next(){
+    this.slider.slickNext();
+}
+previous(){
+    this.slider.slickPrev();
+}
+
+
+
      render() {
           var settings = {
                dots: false,
-               infinite: false,
+               infinite: true,
                speed: 500,
                autoplay: true,
                autoplaySpeed:3000,
@@ -48,13 +63,19 @@ class NewArrival extends Component {
           return ( 
                <Fragment>
           <Container className="text-center" fluid={true}>
-          <div className="section-title text-center mb-55"><h2>NEW ARRIVAL</h2>
+          <div className="section-title text-center mb-55"><h2>NEW ARRIVAL &nbsp;
+
+          <a className="btn btn-sm ml-2 site-btn" onClick={this.previous} ><i className="fa fa-angle-left"></i></a>
+&nbsp;
+<a className="btn btn-sm ml-2 site-btn" onClick={this.next} ><i className="fa fa-angle-right"></i></a>
+
+          </h2>
           <p>Some Of Our Exclusive Collection, You May Like</p>
           </div>
 
                <Row>
 
-               <Slider {...settings}>
+               <Slider ref={c=>(this.slider=c)} {...settings}>
           <div>
           <Card className="image-box card">
           <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/3/03-47-2452-med_5.jpg" />   
@@ -86,46 +107,46 @@ class NewArrival extends Component {
           </div>
           <div>
           <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jw0zr0w0/watch/c/u/r/ls2811-limestone-original-imafgrxqf8qvecjd.jpeg?q=50" />   
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/6/06-93-2911.jpg" />   
           <Card.Body> 
-          <p className="product-name-on-card">Isolation Gown Blue</p>
-          <p className="product-price-on-card">Price : $120</p>
+          <p className="product-name-on-card">Fluticason/Salmeterl Disc Inhaler 100/50</p>
+          <p className="product-price-on-card">Price : $40</p>
           </Card.Body>
           </Card>
           </div>
           <div>
           <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50" />   
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/2/02-70-5212.jpg" />   
         <Card.Body> 
-          <p className="product-name-on-card">Isolation Gown Blue</p>
-          <p className="product-price-on-card">Price : $120</p>
+          <p className="product-name-on-card">Portable Standard Ophthalmoscope</p>
+          <p className="product-price-on-card">Price : $152</p>
           </Card.Body>
           </Card>
           </div>
           <div>
           <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50" />   
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/7/07-71-0800_1.jpg" />   
         <Card.Body> 
-          <p className="product-name-on-card">Isolation Gown Blue</p>
-          <p className="product-price-on-card">Price : $120</p>
+          <p className="product-name-on-card">Oxygen Concentrator 5L</p>
+          <p className="product-price-on-card">Price : $1200</p>
           </Card.Body>
           </Card>
           </div>
           <div>
           <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50" />   
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/2/02-24-0840.jpg" />   
         <Card.Body> 
-          <p className="product-name-on-card">Isolation Gown Blue</p>
-          <p className="product-price-on-card">Price : $120</p>
+          <p className="product-name-on-card">Vital Signs Patient Monitor with Stand</p>
+          <p className="product-price-on-card">Price : $2830</p>
           </Card.Body>
           </Card>
           </div>
           <div>
           <Card className="image-box card">
-          <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50" />   
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/2/02-24-2640_1.jpg" />   
         <Card.Body> 
-          <p className="product-name-on-card">Isolation Gown Blue</p>
-          <p className="product-price-on-card">Price : $120</p>
+          <p className="product-name-on-card">Probe Covers for tymPRO+ Thermometer</p>
+          <p className="product-price-on-card">Price : $300</p>
           </Card.Body>
           </Card>
           </div>
