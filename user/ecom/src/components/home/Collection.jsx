c1f4e1239d1c7fe4fc1a937dbdc7a6f83cf1d772
lizapira <lp47657@ubt-uni.net>
2022-05-22 17:17:31 +0200

    create collection.jsx

---
+++ user/ecom/src/components/home/Collection.jsx
@@ -0,0 +1,207 @@
+import React, { Component, Fragment } from 'react'
+import {Container,Row,Col,Card} from 'react-bootstrap'
+
+class Categories extends Component {
+     render() {
+          return (
+               <Fragment>
+                  <Container className="text-center" fluid={true}>
+          <div className="section-title text-center mb-55"><h2> CATEGORIES</h2>
+          <p>Some Of Our Exclusive Collection, You May Like</p>
+          </div>
+
+          <Row>
+
+
+               <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+           <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />  
+          <h5 className="category-name">Top Offers</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/2/02-20-9005.jpg" />  
+          <h5 className="category-name">Diagnostic Equipment</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/3/03-75-5602-xxl_1.jpg" />  
+          <h5 className="category-name">Infection Control</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col> 
+                    </Row>
+
+               </Col> 
+
+
+
+               <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+           <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/1/1/11-81-0117.jpg" />  
+          <h5 className="category-name">Manikins & Simulators</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/5/05-02-1216.jpg" />  
+          <h5 className="category-name">Patient Care</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col> 
+                    </Row>
+
+               </Col> 
+          </Row>
+
+
+
+
+          <Row>
+
+
+               <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+           <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" />  
+          <h5 className="category-name">Grocery</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" />  
+          <h5 className="category-name">Mobiles</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col> 
+                    </Row>
+
+               </Col> 
+
+
+
+               <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+           <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col>
+
+
+          <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+          <Card className="h-100 w-100 text-center">          
+          <Card.Body> 
+          <img className="center" src="https://www.pocketnurse.com/media/catalog/product/cache/46c67a7e014cd6344be7c367252caa28/0/4/04-25-3332.jpg" />  
+          <h5 className="category-name">Furniture</h5>  
+          </Card.Body>
+          </Card> 
+          </Col> 
+                    </Row>
+
+               </Col> 
+          </Row>
+
+
+                    </Container>
+               </Fragment>
+          )
+     }
+}
+
+export default Categories
