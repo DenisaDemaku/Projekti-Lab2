42704245b18bb1ae64394fc0de36d44882fd4c51
lizapira <lp47657@ubt-uni.net>
2022-05-22 17:18:55 +0200

    update homepage.jsx

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import Categories from '../components/home/Categories'
+import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 
 export class HomePage extends Component {
      render() {
           return (
                <Fragment>
-                   
                     <FeaturedProducts />
-                    <Categories/>
+                    <Collection />
+                    <Categories />
                </Fragment>
           )
      }
