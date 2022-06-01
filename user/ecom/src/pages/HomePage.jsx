9613c279878e6009f199b88d7a9547ff29ba612c
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-06-01 17:36:46 +0200

    made it responsive

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,24 +1,33 @@
-import React, { Component, Fragment } from 'react'
-import NavMenuDesktop from '../components/common/NavMenuDesktop'
-import Categories from '../components/home/Categories'
+import React, { Component, Fragment } from "react";
+import NavMenuDesktop from "../components/common/NavMenuDesktop";
+import Categories from "../components/home/Categories";
 import NavMenuMobile from "../components/common/NavMenuMobile";
-import Collection from '../components/home/Collection'
-import FeaturedProducts from '../components/home/FeaturedProducts'
-import HomeTop from '../components/home/HomeTop'
-import NewArrival from '../components/home/NewArrival'
+import Collection from "../components/home/Collection";
+import FeaturedProducts from "../components/home/FeaturedProducts";
+import HomeTop from "../components/home/HomeTop";
+import NewArrival from "../components/home/NewArrival";
+import HomeTopMobile from "../components/home/HomeTopMobile";
 
 export class HomePage extends Component {
-     render() {
-          return (
-            <Fragment>
-              <NavMenuMobile />
-              <HomeTop />
-              <FeaturedProducts />
-              <NewArrival />
-              <Categories />
-              <Collection />
-            </Fragment>
-          );
-     }
+  render() {
+    return (
+      <Fragment>
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMobile />
+          <HomeTopMobile />
+        </div>
+
+        <FeaturedProducts />
+        <NewArrival />
+        <Categories />
+        <Collection />
+      </Fragment>
+    );
+  }
 }
-export default HomePage
+export default HomePage;
