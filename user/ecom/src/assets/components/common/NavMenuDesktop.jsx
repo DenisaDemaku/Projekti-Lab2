d73ed0a7e300bafcacd321a68b40b2dac59ac5ff
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-06-01 16:40:32 +0200

    Nav menu desktop

--- user/ecom/src/assets/components/common/NavMenuDesktop.jsx
+++ user/ecom/src/assets/components/common/NavMenuDesktop.jsx
@@ -1,36 +1,40 @@
-import React, { Component, Fragment } from 'react'
-import {Navbar,Container, Row, Col} from 'react-bootstrap';
-import Logo from '../../assets/images/medicalsupplies.png';
-import {Link} from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import { Navbar, Container, Row, Col, Button } from "react-bootstrap";
+import Logo from "../../assets/images/medicalsupplies.png";
+import { Link } from "react-router-dom";
 
 class NavMenuDesktop extends Component {
-     render() {
-          return (
-               <Fragment>
-               <div className="TopSectionDown">
-               <Navbar fixed={"top"} className="navbar" bg="light">
-               
-                   <Container fluid={"true"}>
-                        <Row>
-                             <Col lg={4} md={4} sm={12} xs={12}>
-                             <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
-                             </Col>
-               
-                             <Col lg={4} md={4} sm={12} xs={12}>
-                             </Col>
-               
-                             <Col lg={4} md={4} sm={12} xs={12}>
-                             </Col>
-               
-                        </Row>
-               
-                   </Container>
-               
-                 </Navbar>
-                 </div>
-               
-                              </Fragment>
-                         )
-                    }
-               }
-               export default NavMenuDesktop
+  render() {
+    return (
+      <Fragment>
+        <div className="TopSectionDown">
+          <Navbar fixed={"top"} className="navbar" bg="light">
+            <Container
+              fluid={"true"}
+              className="fixed-top shadow-sm p-2 mb-0 bg-white"
+            >
+              <Row>
+                <Col lg={4} md={4} sm={12} xs={12}>
+                  <Link to="/">
+                    {" "}
+                    <img className="nav-logo" src={Logo} />{" "}
+                  </Link>
+                </Col>
+
+                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                  <div className="input-group w-100">
+                    <input type="text" className="form-control" />
+                    <Button type="button" className="btn site-btn">
+                      <i className="fa fa-search"> </i>
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </Navbar>
+        </div>
+      </Fragment>
+    );
+  }
+}
+export default NavMenuDesktop;
