00c05f044d2e11558ba4b994c7a1b4a64c4e0423
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-06-01 16:56:16 +0200

    update on navMenuMobile

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
 import Categories from '../components/home/Categories'
+import NavMenuMobile from "../components/common/NavMenuMobile";
 import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
@@ -8,17 +9,16 @@ import NewArrival from '../components/home/NewArrival'
 
 export class HomePage extends Component {
      render() {
-          return (   
-               <Fragment> 
-                    <NavMenuDesktop />       
-                    <HomeTop />  
-                    <FeaturedProducts />
-                    <NewArrival />
-                    <Categories />
-                    <Collection />
-
-               </Fragment>
-          )
+          return (
+            <Fragment>
+              <NavMenuMobile />
+              <HomeTop />
+              <FeaturedProducts />
+              <NewArrival />
+              <Categories />
+              <Collection />
+            </Fragment>
+          );
      }
 }
 export default HomePage
