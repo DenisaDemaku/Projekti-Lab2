65bb40c0d870a2c0e46bea69252a87ec76d68c97
denisa <73529362+DenisaDemaku@users.noreply.github.com>
2022-06-09 00:36:29 +0200

    admin start

--- admin/adminecom/routes/api.php
+++ admin/adminecom/routes/api.php
@@ -2,18 +2,49 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\VisitorController;
+use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\SiteInfoController;
+use App\Http\Controllers\Admin\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Admin\ProductListController;
+use App\Http\Controllers\Admin\SliderController;
+use App\Http\Controllers\Admin\ProductDetailsController;
+use App\Http\Controllers\Admin\NotificationController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Get Visitor 
+Route::get('/getvisitor',[VisitorController::class, 'GetVisitorDetails']);
+// Contact Page Route
+Route::post('/postcontact',[ContactController::class, 'PostContactDetails']);
+
+// Site Infro Route
+Route::get('/allsiteinfo',[SiteInfoController::class, 'AllSiteinfo']);
+
+// All Category Route
+Route::get('/allcategory',[CategoryController::class, 'AllCategory']);
+
+// ProductList Route
+Route::get('/productlistbyremark/{remark}',[ProductListController::class, 'ProductListByRemark']);
+
+Route::get('/productlistbycategory/{category}',[ProductListController::class, 'ProductListByCategory']);
+
+Route::get('/productlistbysubcategory/{category}/{subcategory}',[ProductListController::class, 'ProductListBySubCategory']);
+
+// Slider Route
+Route::get('/allslider',[SliderController::class, 'AllSlider']);
+
+// Product Details Route
+Route::get('/productdetails/{id}',[ProductDetailsController::class, 'ProductDetails']);
+
+// Notification Route
+Route::get('/notification',[NotificationController::class, 'NotificationHistory']);
+
+// Search Route
+Route::get('/search/{key}',[ProductListController::class, 'ProductBySearch']);
+
+
+
