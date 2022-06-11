909d2379c622bb4dde3f75cc7708bf8fd5b65533
lizapira <lp47657@ubt-uni.net>
2022-06-11 21:03:48 +0200

    contact.jsx

--- user/ecom/src/components/common/Contact.jsx
+++ user/ecom/src/components/common/Contact.jsx
@@ -3,6 +3,41 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
  
 
 export class Contact extends Component {
+
+     constructor(){
+          super();
+          this.state={
+               name:"",
+               email:"",
+               message:""
+          }
+     }
+
+          nameOnChange =(event)=>{
+               let name = event.target.value;
+               // alert(name);
+               this.setState({name:name})
+          }
+
+          emailOnChange =(event)=>{
+               let email = event.target.value;
+               // alert(email);
+               this.setState({email:email})
+          }
+
+          messageOnChange =(event)=>{
+               let message = event.target.value;
+               // alert(message);
+               this.setState({message:message})
+          }
+
+          onFormSubmit =(event)=>{
+               alert("Hello hi");
+               event.prventDefault();
+
+          }
+
+
      render() {
           return (
                <Fragment>
@@ -12,16 +47,18 @@ export class Contact extends Component {
      
                     <Row className="text-center">
              <Col className="d-flex justify-content-center" md={6} lg={6} sm={12} xs={12}>
-     <Form className="onboardForm">
+             <Form onSubmit={this.onFormSubmit} className="onboardForm">
           <h4 className="section-title-login">CONTACT WITH US </h4>
           <h6 className="section-sub-title">Please Contact With Us </h6>
-          <input className="form-control m-2" type="text" placeholder="Enter Mobile Number" />
+        
+          <input onChange={this.nameOnChange} className="form-control m-2" type="text" placeholder="Enter Your Name" />
 
-          <input className="form-control m-2" type="email" placeholder="Enter Email" />
+           <input onChange={this.emailOnChange} className="form-control m-2" type="email" placeholder="Enter Email" />
 
-          <input className="form-control m-2" type="text" placeholder="Enter Your Message" />
-          <Button className="btn btn-block m-2 site-btn-login"> Send </Button>
+           <Form.Control onChange={this.messageOnChange} className="form-control m-2" as="textarea" rows={3} placeholder="Message" />
           
+           <Button type="submit" className="btn btn-block m-2 site-btn-login"> Send </Button>
+    
      </Form>
      
      
@@ -33,7 +70,6 @@ export class Contact extends Component {
             Rr.Fadil Hoxha L.Kalabria Obj.EXDC, Prishtinë
 Email: medicalsupplies@gmail.com
            </p>
-
 
            <iframe src="https://www.google.com/maps/d/u/0/viewer?mid=1_1pH-6o8u3UeIzGtvBZZkPDjnu3NqoIX&ll=42.54827900995879%2C21.095989500000005&z=9" width="550" height="450" styles="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
