2bfc7a5eeb9f4bd3c67b337a7ff58f70c1d176d9
lizapira <lp47657@ubt-uni.net>
2022-06-13 01:23:47 +0200

    about.jsx

--- user/ecom/src/components/others/About.jsx
+++ user/ecom/src/components/others/About.jsx
@@ -1,7 +1,33 @@
 import React, { Component, Fragment } from 'react'
 import { Container,Row,Col, Form,Button } from 'react-bootstrap'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
 
  class About extends Component {
+
+     constructor(){
+          super();
+          this.state={
+               about:""
+          }
+     }
+
+     componentDidMount(){
+          axios.get(AppURL.AllSiteInfo).then(response =>{
+               let StatusCode = response.status;
+               if(StatusCode==200){
+                    let JsonData = (response.data)[0]['about'];
+                    this.setState({about:JsonData});
+               } 
+
+          }).catch(error=>{
+
+          });
+     }
+
+
+
+
      render() {
           return (
                <Fragment>
@@ -10,13 +36,7 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
       <h4 className="section-title-login">About Us Page </h4>
       <p className="section-title-contact">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+      {this.state.about}
       </p>
 
 
