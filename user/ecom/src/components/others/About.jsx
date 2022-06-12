4ec287ed389912434022287ff596ee86837ad908
lizapira <lp47657@ubt-uni.net>
2022-06-12 23:45:09 +0200

    aboutpage.jsx

---
+++ user/ecom/src/components/others/About.jsx
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react'
+import { Container,Row,Col, Form,Button } from 'react-bootstrap'
+
+ class About extends Component {
+     render() {
+          return (
+               <Fragment>
+               <Container>
+                    <Row className="p-2">
+            <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
+      <h4 className="section-title-login">About Us Page </h4>
+      <p className="section-title-contact">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+      </p>
+
+
+                         </Col>
+                    </Row>
+               </Container>
+          </Fragment>
+          )
+     }
+}
+
+export default About
