6c1274502e689c75285f5d076ec397759f588d15
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-06-18 18:56:49 +0200

    site info

--- user/ecom/src/components/others/Purchase.jsx
+++ user/ecom/src/components/others/Purchase.jsx
@@ -1,48 +1,72 @@
-import React, { Component, Fragment } from 'react'
-import { Container,Row,Col, Form,Button } from 'react-bootstrap'
-import AppURL from '../../api/AppURL';
-import axios from 'axios'
-import ReactHtmlParser from 'react-html-parser';
+   import React, { Component, Fragment } from "react";
+   import { Container, Row, Col, Form, Button } from "react-bootstrap";
+   import AppURL from "../../api/AppURL";
+   import axios from "axios";
+   import ReactHtmlParser from "react-html-parser";
+   import { ToastContainer, toast } from "react-toastify";
+   import "react-toastify/dist/ReactToastify.css";
 
-export class Purchase extends Component {
-     constructor(){
-          super();
-          this.state={
-               purchase:""
-          }
+   export class Purchase extends Component {
+     constructor() {
+       super();
+       this.state = {
+         purchase: "",
+       };
      }
 
-     componentDidMount(){
-          axios.get(AppURL.AllSiteInfo).then(response =>{
-               let StatusCode = response.status;
-               if(StatusCode==200){
-                    let JsonData = (response.data)[0]['parchase_guide'];
-                    this.setState({purchase:JsonData});
-               } 
+     componentDidMount() {
+          
+       let SiteInfoPurchase = sessionStorage.getItem("AllSiteInfo");
 
-          }).catch(error=>{
+       if (SiteInfoPurchase == null) {
+         axios
+           .get(AppURL.AllSiteInfo)
+           .then((response) => {
+             let StatusCode = response.status;
+             if (StatusCode == 200) {
+               let JsonData = response.data[0]["parchase_guide"];
+               this.setState({ purchase: JsonData });
 
-          });
+               sessionStorage.setItem("SiteInfoPurchase", JsonData);
+             } else {
+               toast.error("Somthing Went Wrong", {
+                 position: "bottom-center",
+               });
+             }
+           })
+           .catch((error) => {
+             toast.error("Somthing Went Wrong", {
+               position: "bottom-center",
+             });
+           });
+       } // end If Conditon
+       else {
+         this.setState({ purchase: SiteInfoPurchase });
+       }
      }
 
      render() {
-          return (
-               <Fragment>
-               <Container>
-                    <Row className="p-2">
-            <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-      <h4 className="section-title-login">Purchase Page </h4>
-      <p className="section-title-contact">
-      { ReactHtmlParser(this.state.purchase) }
-      </p>
-     
-     
-                         </Col>
-                    </Row>
-               </Container>
-          </Fragment>
-          )
+       return (
+         <Fragment>
+           <Container>
+             <Row className="p-2">
+               <Col
+                 className="shadow-sm bg-white mt-2"
+                 md={12}
+                 lg={12}
+                 sm={12}
+                 xs={12}
+               >
+                 <h4 className="section-title-login">Purchase Page </h4>
+                 <p className="section-title-contact">
+                   {ReactHtmlParser(this.state.purchase)}
+                 </p>
+               </Col>
+             </Row>
+           </Container>
+           <ToastContainer />
+         </Fragment>
+       );
      }
-}
-
+   }
 export default Purchase
