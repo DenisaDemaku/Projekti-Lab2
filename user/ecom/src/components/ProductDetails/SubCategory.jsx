821d0cd956a7ec2affe18c694ee0ebaab115fa82
lizapira <lp47657@ubt-uni.net>
2022-06-20 01:09:09 +0200

    ProductSubCategoryPage.jsx

---
+++ user/ecom/src/components/ProductDetails/SubCategory.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SubCategory extends Component {
+     render() {
+          return (
+               <div>
+
+               </div>
+          )
+     }
+}
+
+export default SubCategory
+  
