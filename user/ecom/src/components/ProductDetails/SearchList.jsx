85c20afabb618fff4724c268421eb6e0df9dcd14
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-06-27 19:37:22 +0200

    Search API

---
+++ user/ecom/src/components/ProductDetails/SearchList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Fragment } from "react";
+
+class SearchList extends Component {
+  render() {
+    return <Fragment></Fragment>;
+  }
+}
+
+export default SearchList;
--- user/ecom/src/components/common/NavMenuDesktop.jsx
+++ user/ecom/src/components/common/NavMenuDesktop.jsx
@@ -7,95 +7,131 @@ import MegaMenuAll from '../home/MegaMenuAll';
  
    
  class NavMenuDesktop extends Component {
+   constructor() {
+     super();
+     this.state = {
+       SideNavState: "sideNavClose",
+       ContentOverState: "ContentOverlayClose",
+       Searchkey: "",
+       SearchRedirectStauts: false,
+     };
+   }
+   SearchOnChange(event) {
+     let Searchkey = event.target.value;
+     // alert(Searchkey);
+     this.setState({ Searchkey: Searchkey });
+   }
 
-     constructor(){
-          super();
-          this.state={
-               SideNavState: "sideNavClose",
-               ContentOverState: "ContentOverlayClose"
-          }
+   SeachOnClick() {
+     if (this.state.Searchkey.length >= 2) {
+       this.setState({ SearchRedirectStauts: true });
      }
+   }
 
+   MenuBarClickHandler = () => {
+     this.SideNavOpenClose();
+   };
 
-     MenuBarClickHandler=()=>{
-          this.SideNavOpenClose();
+   ContentOverlayClickHandler = () => {
+     this.SideNavOpenClose();
+   };
+
+   SideNavOpenClose = () => {
+     let SideNavState = this.state.SideNavState;
+     let ContentOverState = this.state.ContentOverState;
+     if (SideNavState === "sideNavOpen") {
+       this.setState({
+         SideNavState: "sideNavClose",
+         ContentOverState: "ContentOverlayClose",
+       });
+     } else {
+       this.setState({
+         SideNavState: "sideNavOpen",
+         ContentOverState: "ContentOverlayOpen",
+       });
      }
+   };
 
-     ContentOverlayClickHandler=()=>{
-          this.SideNavOpenClose();
-     }
+   render() {
+     return (
+       <Fragment>
+         <div className="TopSectionDown">
+           <Navbar fixed={"top"} className="navbar" bg="light">
+             <Container
+               fluid={"true"}
+               className="fixed-top shadow-sm p-2 mb-0 bg-white"
+             >
+               <Row>
+                 <Col lg={4} md={4} sm={12} xs={12}>
+                   <img
+                     onClick={this.MenuBarClickHandler}
+                     className="bar-img"
+                     src={Bars}
+                   />
 
+                   <Link to="/">
+                     {" "}
+                     <img className="nav-logo" src={Logo} />{" "}
+                   </Link>
+                 </Col>
 
-     SideNavOpenClose=()=>{
-          let SideNavState = this.state.SideNavState;
-          let ContentOverState = this.state.ContentOverState;
-          if(SideNavState==="sideNavOpen"){
-               this.setState({SideNavState:"sideNavClose",ContentOverState:"ContentOverlayClose"})
-
-          }
-          else{
-               this.setState({SideNavState:"sideNavOpen",ContentOverState:"ContentOverlayOpen"})
-          }
-     }
-
-
-     render() {
-          return (
-               <Fragment>
-<div className="TopSectionDown">
-<Navbar fixed={"top"} className="navbar" bg="light">
-
-    <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
-         <Row>
-              <Col lg={4} md={4} sm={12} xs={12}>
-
-              
-              <img onClick={this.MenuBarClickHandler} className="bar-img" src={Bars} />
-
-              <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
-              </Col>
-
-              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                 <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
                    <div className="input-group w-100">
-                   <input type="text" className="form-control" />
-                   <Button type="button" className="btn site-btn"><i className="fa fa-search"> </i> 
-                   </Button>
+                     <input
+                       onChange={this.SearchOnChange}
+                       type="text"
+                       className="form-control"
+                     />
+
+                     <Button
+                       onClick={this.SeachOnClick}
+                       type="button"
+                       className="btn site-btn"
+                     >
+                       <i className="fa fa-search"> </i>
+                     </Button>
                    </div>
-              </Col>
-
-              <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-              
-              <Link to="/favourite" className="btn"><i className="fa h4 fa-heart"></i><sup><span className="badge text-white bg-danger">3</span></sup>                  
+                 </Col>
+                 
+                 <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                   <Link to="/favourite" className="btn">
+                     <i className="fa h4 fa-heart"></i>
+                     <sup>
+                       <span className="badge text-white bg-danger">3</span>
+                     </sup>
                    </Link>
 
-                   <Link to="/notification" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>                  
+                   <Link to="/notification" className="btn">
+                     <i className="fa h4 fa-bell"></i>
+                     <sup>
+                       <span className="badge text-white bg-danger">5</span>
+                     </sup>
                    </Link>
-                   {/*    <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>  */} 
-                   <Link to="/login" className="h4 btn">Login</Link>
-                   
-       <Link to="/cart" className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Link>
-              </Col>
+                   {/*    <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>  */}
+                   <Link to="/login" className="h4 btn">
+                     Login
+                   </Link>
 
-         </Row>
-   
-    </Container>
+                   <Link to="/cart" className="cart-btn">
+                     <i className="fa fa-shopping-cart"></i> 3 Items{" "}
+                   </Link>
+                 </Col>
+               </Row>
+             </Container>
+           </Navbar>
+         </div>
 
-  </Navbar>
-  </div>
+         <div className={this.state.SideNavState}>
+           <MegaMenuAll />
+         </div>
 
-  <div className={this.state.SideNavState}>
-                <MegaMenuAll />
-          </div>
-
-               <div onClick={this.ContentOverlayClickHandler} className={this.state.ContentOverState}>
-
-               </div>
-
-
-
-               </Fragment>
-          )
-     }
-}
+         <div
+           onClick={this.ContentOverlayClickHandler}
+           className={this.state.ContentOverState}
+         ></div>
+       </Fragment>
+     );
+   }
+ }
 
 export default NavMenuDesktop
