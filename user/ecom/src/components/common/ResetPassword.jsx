6e80e2b672dd0cb9d9c6fdf9bf9a95439b414bb7
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-06-27 20:03:29 +0200

    Added Reset Password file

--- user/ecom/src/components/common/ResetPassword.jsx
+++ user/ecom/src/components/common/ResetPassword.jsx
@@ -1,9 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Login from "../../assets/images/login.png";
+import Forget from "../../assets/images/forget.jpg";
 
-class Register extends Component {
+export class ResetPassword extends Component {
   render() {
     return (
       <Fragment>
@@ -25,50 +25,41 @@ class Register extends Component {
                   xs={12}
                 >
                   <Form className="onboardForm">
-                    <h4 className="section-title-login"> USER REGISTER </h4>
+                    <h4 className="section-title-login"> RESET PASSWORD </h4>
+
                     <input
                       className="form-control m-2"
                       type="text"
-                      placeholder="Enter Your Name"
+                      placeholder="Enter Your Pin Code"
                     />
+
                     <input
                       className="form-control m-2"
                       type="email"
                       placeholder="Enter Your Email"
                     />
+
                     <input
                       className="form-control m-2"
                       type="password"
-                      placeholder="Enter Your Password"
+                      placeholder="Your New Password"
                     />
+
                     <input
                       className="form-control m-2"
                       type="password"
                       placeholder="Confirm Your Password"
                     />
+
                     <Button className="btn btn-block m-2 site-btn-login">
                       {" "}
-                      Sing Up{" "}
+                      Reset Password{" "}
                     </Button>
-                    <br></br> <br></br>
-                    <hr />
-                    <p>
-                      {" "}
-                   <p> <b> Forget My Password? </b><Link to="/forget">
-                    <b> Froget Password </b> 
-                    </Link> </p>
-                    <p>
-                      {" "}
-                      <b> Already Have An Account ? </b>
-                      <Link to="/login">
-                        <b> Login </b>{" "}
-                      </Link>{" "}
-                    </p>
                   </Form>
                 </Col>
 
                 <Col className="p-0 Desktop m-0" md={6} lg={6} sm={6} xs={6}>
-                  <img className="onboardBanner" src={Login} />
+                  <img className="onboardBanner" src={Forget} />
                 </Col>
               </Row>
             </Col>
@@ -79,4 +70,4 @@ class Register extends Component {
   }
 }
 
-export default Register;
+export default ResetPassword;
