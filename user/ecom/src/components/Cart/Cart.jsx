902350c7576d4398bbe8ce102a2bf8fb444b214d
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-07-10 19:18:11 +0200

    cart update

--- user/ecom/src/components/Cart/Cart.jsx
+++ user/ecom/src/components/Cart/Cart.jsx
@@ -1,9 +1,68 @@
 import React, { Component, Fragment } from 'react'
 import {Navbar,Container, Row, Col,Button,Card} from 'react-bootstrap';
+import AppURL from '../../api/AppURL';
 import Product1 from '../../assets/images/product/product1.png'
+import axios from 'axios'
 
 class Cart extends Component {
-     render() {
+
+     constructor(){
+          super();
+          this.state={
+               ProductData:[],
+               isLoading:"",
+               mainDiv:"d-none"
+                              
+          }
+     }
+
+
+     componentDidMount(){
+          axios.get(AppURL.CartList(this.props.user.email)).then(response =>{
+               
+               this.setState({ProductData:response.data,isLoading:"d-none",
+               mainDiv:" "});         
+
+          }).catch(error=>{
+
+          });
+     } 
+
+
+     render() { 
+
+          const MyList = this.state.ProductData;
+          const MyView = MyList.map((ProductList,i)=>{
+               return <div>
+               <Card >                
+     <Card.Body>
+     <Row>
+          <Col md={3} lg={3} sm={6} xs={6}>
+               <img className="cart-product-img" src={ProductList.image} />
+          </Col>
+
+          <Col md={6} lg={6} sm={6} xs={6}>
+          <h5 className="product-name">{ProductList.product_name}</h5>
+          <h6> Quantity = {ProductList.quantity} </h6>
+          <p>{ProductList.size} | {ProductList.color}</p>
+          <h6>Price = {ProductList.unit_price} x {ProductList.quantity} = {ProductList.total_price}$</h6>
+          </Col>
+
+          <Col md={3} lg={3} sm={12} xs={12}>
+          
+          <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
+          
+          </Col>
+     </Row>              
+     </Card.Body>               
+</Card>
+
+
+               </div> 
+          })
+
+
+
           return (
               <Fragment>
 
@@ -14,142 +73,10 @@ class Cart extends Component {
 
 
 
-                   <Row>
-
-
+                   <Row> 
      <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
-          <Card >                
-               <Card.Body>
-               <Row>
-                    <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="cart-product-img" src={Product1} />
-                    </Col>
-
-                    <Col md={6} lg={6} sm={6} xs={6}>
-                    <h5 className="product-name">Infrared Thermometer</h5>
-                <h6> Quantity = 05 </h6>
-                <h6>Price = 05 x 50 = 250€</h6>
-                    </Col>
-
-                    <Col md={3} lg={3} sm={12} xs={12}>
-                    <input placeholder="2" className="form-control text-center" type="number" />
-                    <Button className="btn btn-block w-50 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
-                
-                    </Col>
-               </Row>              
-               </Card.Body>               
-          </Card>
-     </Col> 
-
-
-
-
-     <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
-          <Card >                
-               <Card.Body>
-               <Row>
-                    <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="cart-product-img" src={Product1} />
-                    </Col>
-
-                    <Col md={6} lg={6} sm={6} xs={6}>
-                    <h5 className="product-name">Infrared Thermometer</h5>
-                <h6> Quantity = 05 </h6>
-                <h6>Price = 05 x 50 = 250€</h6>
-                    </Col>
-
-                    <Col md={3} lg={3} sm={12} xs={12}>
-                    <input placeholder="2" className="form-control text-center" type="number" />
-                    <Button className="btn btn-block w-50 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
-                
-                    </Col>
-               </Row>              
-               </Card.Body>               
-          </Card>
-     </Col> 
-
-
-
-     <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
-          <Card >                
-               <Card.Body>
-               <Row>
-                    <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="cart-product-img" src={Product1} />
-                    </Col>
-
-                    <Col md={6} lg={6} sm={6} xs={6}>
-                    <h5 className="product-name">Infrared Thermometer</h5>
-                <h6> Quantity = 05 </h6>
-                <h6>Price = 05 x 50 = 250€</h6>
-                    </Col>
-
-                    <Col md={3} lg={3} sm={12} xs={12}>
-                    <input placeholder="2" className="form-control text-center" type="number" />
-                    <Button className="btn btn-block w-50 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
-                
-                    </Col>
-               </Row>              
-               </Card.Body>               
-          </Card>
-     </Col> 
-
-
-
-
-     <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
-          <Card >                
-               <Card.Body>
-               <Row>
-                    <Col md={3} lg={3} sm={6} xs={6}>
-                         <img className="cart-product-img" src="https://cdn.shopify.com/s/files/1/0012/8440/7394/products/prestige-clinical-lite-stethoscope-prestige-medical-14243851272290_590x700.jpg?v=1628562696" />
-                    </Col>
-
-                    <Col md={6} lg={6} sm={6} xs={6}>
-                    <h5 className="product-name">Stethoscope</h5>
-                <h6> Quantity = 02 </h6>
-                <h6>Price = 05 x 20 = 50€</h6>
-                    </Col>
-
-                    <Col md={3} lg={3} sm={12} xs={12}>
-                    <input placeholder="2" className="form-control text-center" type="number" />
-                    <Button className="btn btn-block w-50 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
-                
-                    </Col>
-               </Row>              
-               </Card.Body>               
-          </Card>
-     </Col> 
-
-
-
-
-
-
-     <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
-          <Card >                
-               <Card.Body>
-               <Row>
-                     
-
-     <Col md={4} lg={4} sm={6} xs={6}>
-         
-                <h5> Total Item = 05 </h5>
-                <h5>Total Price = 250€</h5>
-                <Button className="btn btn-block w-50 mt-3  site-btn"><i className="fa fa-shopping-cart"></i> CheckOut </Button>
-                    </Col> 
-               </Row>              
-               </Card.Body>               
-          </Card>
-     </Col> 
-
-
-
-
-
-
-
-
+         {MyView}
+     </Col>  
                    </Row>
 
                </Container>
